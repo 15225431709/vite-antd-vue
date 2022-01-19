3a3ea48ad596944c856246be08979b444152b619
houz <729643662@qq.com>
2022-01-19 23:06:49 +0800

    23点06分

---
+++ src/pages/Form/step/info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      current: 0,
+    });
+    return {
+        ...toRefs(data)
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+
+</style>
